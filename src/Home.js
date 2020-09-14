91d0f5446e8eb8e57dde3fe00113088e549091b4
Shubham <shubhamnagpal30@gmail.com>
2020-09-14 11:06:26 +0530

    Add product details using props

--- src/Home.js
+++ src/Home.js
@@ -12,22 +12,83 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="12321341"
+            title="Apple iPhone 11 Pro Max (512GB) - Midnight Green"
+            price={99990}
+            image="https://images-na.ssl-images-amazon.com/images/I/61ers6OzvUL._SX679_.jpg"
+            rating={4}
+          />
+          <Product
+            id="49538094"
+            title="Apple AirPods with Wireless Charging Case"
+            price={24999}
+            image="https://images-na.ssl-images-amazon.com/images/I/71zny7BTRlL._SX679_.jpg"
+            rating={3}
+          />
+          <Product
+            id="4903850"
+            title="Apple Watch Series 5 (GPS, 44mm) - Gold Aluminium Case with Pink Sport Band"
+            price={43900}
+            image="https://images-na.ssl-images-amazon.com/images/I/71H8iifAnzL._SX679_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            id="12312312"
+            title="Marshall Uxbridge Home Voice Speaker with Amazon Alexa Built-in,White"
+            price={21999}
+            image="https://images-na.ssl-images-amazon.com/images/I/81WIcjaGp2L._SY450_.jpg"
+            rating={4}
+          />
+          <Product
+            id="32132132"
+            title="Apple MacBook Pro (16-inch, 16GB RAM, 512GB Storage, 2.6GHz 9th Gen Intel Core i7) - Space Grey
+"
+            price={199900}
+            image="https://images-na.ssl-images-amazon.com/images/I/71L2iBSyyOL._SX679_.jpg"
+            rating={5}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            id="90829332"
+            title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+            price={55999}
+            rating={4}
+            image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            id="12612612"
+            title="Nike Men's Black Training Shoes - 8.5 D(M) US"
+            price={12999}
+            image="https://images-na.ssl-images-amazon.com/images/I/61JiTAo1HWL._UX625_.jpg"
+            rating={4}
+          />
+          <Product
+            id="12512512"
+            title="ASUS ROG Strix GL10CS Intel Core i5-9400F 9th Gen Gaming Desktop (8GB RAM/1TB HDD/Windows 10/2GB NVIDIA GeForce GTX 1050 Graphics/Iron Gray), GL10CS-IN063T"
+            price={52999}
+            image="https://images-na.ssl-images-amazon.com/images/I/71JEcduJrXL._SY450_.jpg"
+            rating={4}
+          />
+          <Product
+            id="78978978"
+            title="OnePlus 8 Pro (Onyx Black 8GB RAM+128GB Storage)"
+            price={52999}
+            image="https://images-na.ssl-images-amazon.com/images/I/61YSMhOd5EL._SX679_.jpg"
+            rating={4}
+          />
+          <Product
+            id="45645648"
+            title="Microsoft Xbox One Wireless Controller with Bluetooth (With 3.5 mm Jack) (White)"
+            price={5999}
+            image="https://images-na.ssl-images-amazon.com/images/I/61cIz9OsQOL._SX679_.jpg"
+            rating={4}
+          />
         </div>
       </div>
     </div>
--- src/Product.js
+++ src/Product.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './Product.css';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>This is product 1</p>
+        <p>{title}</p>
         <p className="product__price">
-          <small>₹</small>
-          <strong>1,200</strong>
+          <small>₹ </small>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
-        </div>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>  
+            ))}
+        </div>  
       </div>
-      <img 
-        className=""
-        src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-select-2019-family?wid=882&amp;hei=1058&amp;fmt=jpeg&amp;qlt=80&amp;op_usm=0.5,0.5&amp;.v=1586586488946"
-        alt=""
-      />
+      <img className="" src={image} alt="" />
       <button>Add to Basket</button>
     </div>
   );
 }
 
-export default Product
+export default Product;
