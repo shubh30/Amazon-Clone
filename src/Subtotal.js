642c2b0e28dc4983259f7469fecb9e6b5f223157
Shubham <shubhamnagpal30@gmail.com>
2020-09-15 14:37:57 +0530

    Add subtotal values Dynamic

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from './StateProvider';
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="subtotal">
       <CurrencyFormat
@@ -10,7 +13,7 @@ function Subtotal() {
           <>
             <p>
               {/* Part of the homework */}
-              Subtotal (0 items): <strong>0</strong>
+              Subtotal ({basket?.length} items): <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input type="checkbox" /> This order contains a gift
@@ -18,10 +21,10 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0} // Part of the homework
+        value={getBasketTotal(basket)} // Part of the homework
         displayType={'text'}
         thousandSeparator={true}
-        prefix={'$'}
+        prefix={'₹'}
       />  
 
       <button>Proceed to Checkout</button>
