b0eba5b7c0c742a906fc280e5771bef21e978bc7
Shubham <shubhamnagpal30@gmail.com>
2020-09-16 17:39:13 +0530

    Add firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // This will only run once when the app component loads
+    
+    auth.onAuthStateChanged(authUser => {
+      console.log('USER >>> ', authUser);
+
+      if(authUser) {
+        // The user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // The user is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null   
+        })
+      }
+    })
+  }, []);
+
   return (
     // BEM
     <Router>
